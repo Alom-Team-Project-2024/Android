88b11f8bf8aad177001302d34ff82c87beb6b7de
dojes <bjh59629@naver.com>
2024-07-11 09:46:32 +0900

    하단 네비게이션 의존성 삭제

--- app/src/main/java/com/example/login/LoginActivity.kt
+++ app/src/main/java/com/example/login/LoginActivity.kt
@@ -1,25 +1,13 @@
 package com.example.login
 
-import android.animation.Animator
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.animation.AccelerateDecelerateInterpolator
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.login.databinding.ActivityLoginBinding
-import com.example.login.databinding.ActivityMainBinding
-import com.example.login.databinding.RadialMenuBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var radialMenuBinding: RadialMenuBinding
-    private var isMenuOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
